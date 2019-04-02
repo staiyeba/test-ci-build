e830bb6c441939d8f2b716bcd656795cabd9ca1d
taiyeba <staiyeba@gmail.com>
2019-04-02 16:36:38 +0200

    testing final stage

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,5 +5,6 @@ set -x
 #ls -A | grep -v src | xargs rm -r || :
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     echo "building for macos"
-    brew install grep
+    brew install python3 || :
 fi
+pip3 install conan --upgrade
