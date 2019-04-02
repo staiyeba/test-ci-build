ab23947882fb39dc8380c65311764ec6b103179d
taiyeba <staiyeba@gmail.com>
2019-04-02 16:43:59 +0200

    test

--- dependencies/gnu/binutils/ca.sh
+++ dependencies/gnu/binutils/ca.sh
@@ -2,9 +2,4 @@
 
 set -e
 set -x
-echo "running ca"
-echo "making changes to ca"
-echo "making advanced changes"
-echo "i think the check can be tried now"
-echo "making a change here should now run full job? nooo"
-echo "should work now"
+echo "testing is awesome"
