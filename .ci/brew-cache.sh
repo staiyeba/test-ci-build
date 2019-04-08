f08e0baacf5d45878119b5e690cefe04ee24dca7
taiyeba <staiyeba@gmail.com>
2019-04-08 13:37:35 +0200

    testing: homebrew cache

---
+++ .ci/brew-cache.sh
@@ -0,0 +1,6 @@
+i#!/bin/sh
+
+mkdir travis-cache
+if [ ! -f homebrew-cache.tar.gz ]; then
+  tar czf travis-cache/homebrew-cache.tar.gz --directory /usr/local/Cellar grep python3 conan
+fi
--- .ci/install.sh
+++ .ci/install.sh
@@ -5,6 +5,6 @@ set -x
 #ls -A | grep -v src | xargs rm -r || :
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     echo "building for macos"
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install python3 || :
+    brew install python3 || :
 fi
 pip3 install conan --upgrade
