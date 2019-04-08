598143a11da46a74a70c36798cee492e5f80ef74
taiyeba <staiyeba@gmail.com>
2019-04-08 13:22:10 +0200

    fixing no update command

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,6 +5,6 @@ set -x
 #ls -A | grep -v src | xargs rm -r || :
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     echo "building for macos"
-    brew install python3 || :
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install python3 || :
 fi
 pip3 install conan --upgrade
