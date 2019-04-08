7f2a35b59b2184c0857f3cc2a85b064dbe7b1a6e
taiyeba <staiyeba@gmail.com>
2019-04-08 14:02:57 +0200

    brew-cache tar issues fixing

--- .ci/brew-cache.sh
+++ .ci/brew-cache.sh
@@ -2,5 +2,5 @@
 
 mkdir travis-cache
 if [ ! -f homebrew-cache.tar.gz ]; then
-  tar czf travis-cache/homebrew-cache.tar.gz --directory /usr/local/Cellar grep python3 conan
+  tar -czf travis-cache/homebrew-cache.tar.gz --directory /usr/local/Cellar grep python3 conan
 fi
