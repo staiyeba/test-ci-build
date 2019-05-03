ceb42e5302baaf30c01c4e0360e26ea5c9aee3a9
taiyeba <staiyeba@gmail.com>
2019-05-03 14:51:35 +0200

    simplify

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,6 +5,4 @@ set -x
 #ls -A | grep -v src | xargs rm -r || :
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     echo "building for macos"
-    brew install python3 || :
 fi
-pip3 install conan --upgrade
