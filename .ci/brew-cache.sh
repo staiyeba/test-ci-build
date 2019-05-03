78cb409147b5742bc0f93af5c96203206b563753
taiyeba <staiyeba@gmail.com>
2019-05-03 14:58:06 +0200

    simplify to test trigger

--- .ci/brew-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir travis-cache
-if [ ! -f homebrew-cache.tar.gz ]; then
-  tar -czf travis-cache/homebrew-cache.tar.gz --directory /usr/local/Cellar grep python conan
-fi
--- .ci/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-#ls -A | grep -v src | xargs rm -r || :
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    echo "building for macos"
-fi
